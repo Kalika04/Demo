994b049c877b844704f57770f8c5b710c39f4115
Kalika04 <2022pietcdkalika030@poornima.org>
2025-06-13 15:31:35 +0530

    total python and pandas

---
+++ day2.py
@@ -0,0 +1,50 @@
+# print("hello")
+# print("world")
+ 
+# a=2
+# b=3
+# print(a)
+
+# name ="kalika"
+# age=36
+# print(name)
+# print(age)
+
+# 5=5
+# print(5)
+
+# 5==5
+# print(5)
+
+# print="hello python"
+# print(print)
+
+
+# kalika="hello python"
+# print(kalika)
+
+# name ="kalika"
+# surname ="mathur"
+# print(name+ " "+surname)
+
+
+
+#INDEXING
+# name ='kalika'
+# print(name[0])
+# print(name[3])
+
+#STRING
+
+# print(name[0:4])
+
+# city="jaipur"
+# company_name="upflairs"
+# print("hello i am from {city}and my company is{company_name}")
+
+
+
+# <<<<<<<<<<<<LIST>>>>>>>>>>>>>>>>>>>>>>>
+
+ls=[1,2,4,4,2,5,8,6,3,24,5,4,45,"kalika", 'mathur']
+print(ls)
---
+++ day3.py
@@ -0,0 +1,150 @@
+# <<<<<<<<<<TUPLE>>>>>>>>>>>>>>>>>>>
+# ordered and unchangable
+# hetrogenous
+# allow duplicate values
+
+# tpl = (1,2,3,4,5,"hello", "world",2.5)
+# # print(tpl)
+# # print ("types of tuple:", type(tpl))
+# # print("length of tuple:", len(tpl))
+
+# # Slicing
+# print(tpl[0:5])
+
+# tpl =1,2,5,54,5,5,5
+# print(tpl)
+# print('length of tpl:', len(tpl))
+# print(type(tpl))
+
+
+# Tuple unpacking
+# kapil=1,2,3
+# print(kapil)
+# a,b,c = kapil
+# print(a)
+# print(b)
+# print(c)
+
+# t1 =(1,2)
+# t2 =(3,4,2)
+# # print(t1+t2)
+# print(t2*3)
+
+# t2=(3,4,2,(1,2))
+# print(len(t2))
+
+# print(t2[3][0])
+
+# t=(1,2,3,4,5,5,45,7,8,8)
+# print(t.index(2))
+# print(t.count(5))/
+# t[0]=100
+# print(t)
+
+# task1 ek tpl us list : t=(1,2,3,4,5,5,[45,7,8,8])
+
+
+
+# <<<<<<<<<<DICTIONARY>>>>>>>>>>>>>>>>>>>>
+
+# Uordered
+# hetrogrnous
+# allow duplicate values but not key
+
+# person ={
+#    "name": "hemant", "age": 22, "city": "jaipur"
+#  }
+# # print(person)
+# print(type(person))
+# print(person["name"])
+# print(person["age"])
+# print(person["city"])
+
+# adding new key value pair
+
+# person["country"]="india"
+# print(person)
+
+
+#  task 2 updating values of dict
+
+## removing key value pair from dict
+# person.pop["country"]
+# print(person)
+
+
+# dict methods
+
+# print("keys of dict:",person.keys())
+# print("values of dict;",person.values())
+# print("items of dict:",person.items())
+
+
+
+# >>>>>>>>>>>SET>>>>>>>>>>>>>>>>>>>>>
+# unordered and unindexed
+# hetrogenous
+# no duplicate
+# fast membership test
+
+# my_set= {1,2,3,4,5}
+# print(type(my_set))
+# print(my_set)
+
+# my_set={1,2,3,4,5,5}
+# print(my_set)
+
+# empty set
+# my_set=set()
+# print(type(my_set))
+
+# my_set={1,2,3,4,5,5}
+# my_set.add(6)
+# print(my_set)
+# my_set.remove(6)
+# print(my_set)
+# my_set.discard(6)
+# print(my_set)
+# a={1,2,3}
+# b={3,4,5}
+# print (a.union(b))
+# print(a.difference)
+# print(b.difference)
+
+# membership in set
+# my_set ={1,2,3,4,5}
+# print(1 in my_set)
+# print(5 in my_set)
+
+# type casting
+# name =[1,2,3,4,5]
+# print(name)
+# print(type(name))
+
+# # convert into set
+# name
+
+# task 1
+
+# student={"name":"kalika", "age": 20,"city" :"jaipur"}
+# student= ["age"] =21 
+# student=["city"] = "udaipur" 
+# print(student)
+
+# task2
+
+# t = (1, 2, 3, 4, 5, 5,[45, 7, 8, 8])
+# print(t[6][1]) 
+
+
+#task 3
+
+# a=12
+# b=10
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
